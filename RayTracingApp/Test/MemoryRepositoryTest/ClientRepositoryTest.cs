73ddf55667fb1fb83d440180a44089061327ade1
Renzo-Test <renzotestorelli27@gmail.com>
2023-04-01 18:20:28 -0300

    red - createClientRepository_OkTest

---
+++ RayTracingApp/Test/MemoryRepositoryTest/ClientRepositoryTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using MemoryRepository;
+
+namespace Test.MemoryRepository
+{
+    [TestClass]
+    public class ClientRepositoryTest
+    {
+        [TestMethod]
+        public void createClientList_OkTest()
+        {
+            ClientRepository clientRepository = new ClientRepository();
+        }
+    }
+}
