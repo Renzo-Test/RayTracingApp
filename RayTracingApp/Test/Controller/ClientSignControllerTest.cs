b3315cd9e289230b663136888ae6e6fc0fe712af
MateoGiraz <mateogiraz27@gmail.com>
2023-04-03 03:00:49 -0300

    red - CanCreateClientSignController_OkTest

---
+++ RayTracingApp/Test/Controller/ClientSignControllerTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.Controller
+{
+    [TestClass]
+    public class ClientSignControllerTest
+    {
+        private ClientSignController _controller;
+
+        [TestMethod]
+        public void CanCreateClientSignController_OkTest()
+        {
+            _controller = new ClientSignController();
+        }
+    }
+}
