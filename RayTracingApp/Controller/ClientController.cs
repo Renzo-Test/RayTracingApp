e12282b55f81bb12b5396398f4955a7f57c0ad4e
MateoGiraz <mateogiraz27@gmail.com>
2023-04-03 00:44:22 -0300

    red - canCreateClientPasswordController_OkTest

---
+++ RayTracingApp/Controller/ClientController.cs
@@ -0,0 +1,15 @@
+﻿using IRepository;
+using MemoryRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controller
+{
+    public abstract class ClientController
+    {
+        protected IRepositoryClient _repository = new ClientRepository();
+    }
+}
---
+++ RayTracingApp/Test/Controller/ClientPasswordControllerTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.Controller
+{
+    [TestClass]
+    public class ClientPasswordControllerTest
+    {
+        private ClientPasswordController _controller;
+
+        [TestMethod]
+        public void canCreateClientPasswordController_OkTest()
+        {
+            _controller = new ClientPasswordController();
+        }
+    }
+}
