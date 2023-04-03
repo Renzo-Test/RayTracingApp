1e442d48b40a485de06ec806e39b949cd5d9bcfa
MateoGiraz <mateogiraz27@gmail.com>
2023-04-03 01:39:28 -0300

    red - CanCreateClientUsernameController_OkTest

---
+++ RayTracingApp/Test/Controller/ClientUsernameControllerTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Controller;
+
+namespace Test.Controller
+{
+    [TestClass]
+
+    public class ClientUsernameControllerTest
+    {
+        private ClientUsernameController _controller;
+
+        [TestMethod]
+        public void CanCreateClientUsernameController_OkTest()
+        {
+            _controller = new ClientUsernameController();
+        }
+
+    }
+}
