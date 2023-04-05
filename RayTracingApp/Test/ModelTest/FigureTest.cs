b5e15a3584e6a1d0de996aa35e2bf3ce8200083f
SanJingSung <santicapoactualizado2@gmail.com>
2023-04-05 20:32:58 -0300

    red-canCreateFigure_OkTest

---
+++ RayTracingApp/Test/ModelTest/FigureTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Test.Model
+{
+    [TestClass]
+    public class FigureTest
+    {
+        [TestMethod]
+        public void CanCreateFigure_OkTest()
+        {
+            Figure figure = new Figure();
+        }
+    }
+}
