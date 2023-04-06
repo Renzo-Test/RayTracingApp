f24e03b971e0bda23008c573605789bc65d76787
Renzo-Test <renzotestorelli27@gmail.com>
2023-04-06 13:16:29 -0300

    red - CreateFigureRepository_OkTest

---
+++ RayTracingApp/Test/MemoryRepositoryTest/FigureRepositoryTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Test.MemoryRepositoryTest
+{
+    [TestClass]
+    public class FigureRepositoryTest
+    {
+        [TestMethod]
+        public void CreateFigureRepository_OkTest()
+        {
+            FigureRepository _figureRepository = new FigureRepository();
+        }
+    }
+}
