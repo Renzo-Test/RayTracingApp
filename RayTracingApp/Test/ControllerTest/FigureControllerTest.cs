6676635250ee30b0ba9c8c763de08ebeff14b716
Renzo-Test <renzotestorelli27@gmail.com>
2023-04-06 15:08:23 -0300

    red - CreateFigureController_OkTest

---
+++ RayTracingApp/Test/ControllerTest/FigureControllerTest.cs
@@ -0,0 +1,18 @@
+﻿using Controller;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Test.ControllerTest
+{
+    [TestClass]
+    public class FigureControllerTest
+    {
+        private FigureController _figureController;
+
+        [TestMethod]
+        public void CreateFigureController_OkTest()
+        {
+            _figureController = new FigureController();
+        }
+    }
+}
