ad74a36b43b9983b07e8829ae3c637d267acf9b7
Renzo-Test <renzotestorelli27@gmail.com>
2023-04-06 16:15:33 -0300

    green - CheckNameIsNotEmpty_FigureName_OkTest

--- RayTracingApp/Controller/FigureController.cs
+++ RayTracingApp/Controller/FigureController.cs
@@ -10,7 +10,7 @@ namespace Controller
     {
         public bool CheckNameIsNotEmpty(string figureName)
         {
-            return false;
+            return !string.IsNullOrEmpty(figureName);
         }
     }
 }
