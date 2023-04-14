0214202a822d904f2e87cc6e678fb5a47f57fef5
MateoGiraz <mateogiraz27@gmail.com>
2023-04-14 19:20:08 -0300

    red - RemoveMaterial_OkTest

--- RayTracingApp/Test/MemoryRepositoryTest/MaterialRepositoryTest.cs
+++ RayTracingApp/Test/MemoryRepositoryTest/MaterialRepositoryTest.cs
@@ -2,6 +2,7 @@
 using System;
 using MemoryRepository.MaterialRepository;
 using Model;
+using System.Collections.Generic;
 
 namespace Test.MemoryRepositoryTest
 {
@@ -58,6 +59,34 @@ namespace Test.MemoryRepositoryTest
             };
 
             _materialRepository.AddMaterial(NewMaterial);
+
+        }
+
+        [TestMethod]
+        public void RemoveMaterial_OkTest()
+        {
+            MaterialRepository _materialRepository = new MaterialRepository();
+
+            Color NewColor = new Color()
+            {
+                Red = 222,
+                Green = 222,
+                Blue = 222,
+            };
+
+            Material NewMaterial = new LambertianMaterial()
+            {
+                Name = "Test",
+                Owner = "OwnerName",
+                Color = NewColor,
+            };
+
+
+            _materialRepository.AddMaterial(NewMaterial);
+            _materialRepository.RemoveMaterial(NewMaterial);
+            List<Material> iterable = _materialRepository.GetMaterialsByClient("OwnerName");
+
+            CollectionAssert.DoesNotContain(iterable, NewMaterial);
 
         }
 
