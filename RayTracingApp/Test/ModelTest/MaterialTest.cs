d6e2a92c63e524a9e7143fd16875095822db28e1
MateoGiraz <mateogiraz27@gmail.com>
2023-04-14 17:15:43 -0300

    red - CanCreateMaterial_OkTest

---
+++ RayTracingApp/Test/ModelTest/MaterialTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Model;
+
+namespace Test.ModelTest
+{
+    [TestClass]
+
+    public class Class1
+    {
+        [TestMethod]
+        public void CanCreateMaterial_OkTest()
+        {
+            Material _material = new Material();
+        }
+    }
+}
