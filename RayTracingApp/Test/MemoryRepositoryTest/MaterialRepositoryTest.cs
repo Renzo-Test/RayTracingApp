08afcd0e596ed6629d3096a6a1e1ba043169021f
MateoGiraz <mateogiraz27@gmail.com>
2023-04-14 18:58:09 -0300

    red - CreateMaterialRepository_OkTest

---
+++ RayTracingApp/Test/MemoryRepositoryTest/MaterialRepositoryTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using MemoryRepository;
+
+namespace Test.MemoryRepositoryTest
+{
+    [TestClass]
+    public class Class1
+    {
+        [TestMethod]
+        public void CreateMaterialRepository_OkTest()
+        {
+            MaterialRepository _materialRepository = new MaterialRepository();
+        }
+    }
+}
