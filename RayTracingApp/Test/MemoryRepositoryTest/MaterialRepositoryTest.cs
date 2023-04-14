8ebc46b62045dc8fd6e3ef7faabce722187326c8
MateoGiraz <mateogiraz27@gmail.com>
2023-04-14 19:07:57 -0300

    red - GetFiguresByClient_OwnerName_OkTest

--- RayTracingApp/Test/MemoryRepositoryTest/MaterialRepositoryTest.cs
+++ RayTracingApp/Test/MemoryRepositoryTest/MaterialRepositoryTest.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using MemoryRepository.MaterialRepository;
+using Model;
 
 namespace Test.MemoryRepositoryTest
 {
@@ -11,6 +12,30 @@ namespace Test.MemoryRepositoryTest
         public void CreateMaterialRepository_OkTest()
         {
             MaterialRepository _materialRepository = new MaterialRepository();
+        }
+
+        [TestMethod]
+        public void GetFiguresByClient_OwnerName_OkTest()
+        {
+            MaterialRepository _materialRepository = new MaterialRepository();
+
+            Color NewColor = new Color()
+            {
+                Red = 222,
+                Green = 222,
+                Blue = 222,
+            };
+
+            Material NewMaterial = new LambertianMaterial()
+            {
+                Name = "Test",
+                Owner = "OwnerName",
+                Color = NewColor,
+            };
+
+            _materialRepository.AddMaterial(NewMaterial);
+
+            Assert.AreEqual(NewMaterial, _materialRepository.GetMaterialsByClient("OwnerName")[0]);
         }
     }
 }
