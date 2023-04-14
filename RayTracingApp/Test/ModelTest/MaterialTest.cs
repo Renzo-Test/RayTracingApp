092ed30af215173e0b44754dae7b1c0b0a8f97b9
MateoGiraz <mateogiraz27@gmail.com>
2023-04-14 17:32:41 -0300

    red - SetOwner_Gomez_OkTest

--- RayTracingApp/Test/ModelTest/MaterialTest.cs
+++ RayTracingApp/Test/ModelTest/MaterialTest.cs
@@ -8,10 +8,22 @@ namespace Test.ModelTest
 
     public class Class1
     {
+        private Material _material;
+
         [TestMethod]
         public void CanCreateMaterial_OkTest()
         {
-            Material _material = new Material();
+            _material = new Material();
+        }
+
+        [TestMethod]
+        public void SetOwner_Gomez_OkTest()
+        {
+            _material = new Material()
+            {
+                Owner = "Gomez",
+            };
+            Assert.AreEqual("Gomez", _material.Owner);
         }
     }
 }
