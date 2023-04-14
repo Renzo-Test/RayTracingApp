c249c9068eba80cd5ed1dc9ef7a5b21311f2aa9c
MateoGiraz <mateogiraz27@gmail.com>
2023-04-14 19:12:04 -0300

    green - AddMaterial_OkTest

--- RayTracingApp/MemoryRepository/MaterialRepository/MaterialRepository.cs
+++ RayTracingApp/MemoryRepository/MaterialRepository/MaterialRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,16 @@ namespace MemoryRepository.MaterialRepository
 {
     public class MaterialRepository
     {
+        private List<Material> _materials;
+
+        public MaterialRepository()
+        {
+            _materials = new List<Material>();
+        }
+
+        public void AddMaterial(Material newMaterial)
+        {
+            _materials.Add(newMaterial);
+        }
     }
 }
