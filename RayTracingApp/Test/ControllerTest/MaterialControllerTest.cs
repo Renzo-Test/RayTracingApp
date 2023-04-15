908ec65609594399ba4535cb04756b7dc0af4f8a
Renzo-Test <renzotestorelli27@gmail.com>
2023-04-14 22:16:13 -0300

    red - CreateClientController

---
+++ RayTracingApp/Test/ControllerTest/MaterialControllerTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Model;
+
+namespace Test.ControllerTest
+{
+    [TestClass]
+    public class MaterialControllerTest
+    {
+        [TestMethod]
+        public void CreateClientController_OkTest()
+        {
+            _materialController = new MaterialController();
+        }
+    }
+}
