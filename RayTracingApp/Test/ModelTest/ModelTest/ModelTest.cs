bc328f8f39b55144ebca5daf782b51aad30d1269
Renzo-Test <renzotestorelli27@gmail.com>
2023-04-15 18:42:50 -0300

    red - CreateModel

---
+++ RayTracingApp/Test/ModelTest/ModelTest/ModelTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Test.ModelTest.ModelTest
+{
+    [TestClass]
+    public class ModelTest
+    {
+        [TestMethod]
+        public void CreateModel_OkTest()
+        {
+            Model = new Model();
+        }
+    }
+}
