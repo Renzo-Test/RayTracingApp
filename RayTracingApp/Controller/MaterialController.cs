b99b4387426ccf67a5fe8cd188a2055a8c53ee4e
Renzo-Test <renzotestorelli27@gmail.com>
2023-04-14 22:23:50 -0300

    green - CreateClientController

---
+++ RayTracingApp/Controller/MaterialController.cs
@@ -0,0 +1,20 @@
+﻿using IRepository;
+using MemoryRepository.MaterialRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controller
+{
+    public class MaterialController
+    {
+        public IRepositoryMaterial Repository;
+
+        public MaterialController()
+        {
+            Repository = new MaterialRepository();
+        }
+    }
+}
--- RayTracingApp/Test/ControllerTest/MaterialControllerTest.cs
+++ RayTracingApp/Test/ControllerTest/MaterialControllerTest.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using Model;
+using Controller;
 
 namespace Test.ControllerTest
 {
     [TestClass]
     public class MaterialControllerTest
     {
+        private MaterialController _materialController;
+
         [TestMethod]
         public void CreateClientController_OkTest()
         {
