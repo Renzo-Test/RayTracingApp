8d9055d94f4b87e2e31784326c7a455f91f02143
Santiago <santicapoactualizado2@gmail.com>
2023-04-17 20:08:38 -0300

    red-CreateModelRepository_OkTest

---
+++ RayTracingApp/Test/MemoryRepositoryTest/ModelRepositoryTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Models;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using MemoryRepository.Exceptions;
+
+namespace Test.MemoryRepositoryTest
+{
+    [TestClass]
+    public class ModelRepositoryTest
+    {
+        [TestMethod]
+        public void CreateModelRepositoryOk_Test()
+        {
+            ModelRepository modelRepository = new ModelRepository();
+        }
+    }
+}
