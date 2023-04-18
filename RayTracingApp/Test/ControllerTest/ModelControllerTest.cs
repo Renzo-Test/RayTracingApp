b42e1f7f78fbe6ee7a68fba5f40c5b1ab332df59
Renzo-Test <renzotestorelli27@gmail.com>
2023-04-17 23:50:21 -0300

    red - CreateModelController

---
+++ RayTracingApp/Test/ControllerTest/ModelControllerTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Controller;
+
+namespace Test.ControllerTest
+{
+    [TestClass]
+    public class ModelControllerTest
+    {
+        [TestMethod]
+        public void CreateModelController_OkTest()
+        {
+            ModelController controller = new ModelController();
+        }
+    }
+}
