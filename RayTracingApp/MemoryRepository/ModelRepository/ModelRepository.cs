b011bfe60416c5463338b57befa62392d0cb575e
Santiago <santicapoactualizado2@gmail.com>
2023-04-17 21:37:18 -0300

    green-AddModel_OkTest

--- RayTracingApp/MemoryRepository/ModelRepository/ModelRepository.cs
+++ RayTracingApp/MemoryRepository/ModelRepository/ModelRepository.cs
@@ -16,6 +16,9 @@ namespace MemoryRepository
         {
             _models = new List<Model>();
         }
+        public void AddModel(Model newModel)
+        {
+        }
         public List<Model> GetModelsByClient(string username) 
         {
             List<Model> foundModels = _models.FindAll(model => model.Owner.Equals(username));
