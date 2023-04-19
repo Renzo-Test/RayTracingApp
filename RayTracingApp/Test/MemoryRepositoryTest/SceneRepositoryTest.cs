9b1045d3aebbf2fa2d154e6692e25bc03938ff25
MateoGiraz <mateogiraz27@gmail.com>
2023-04-19 15:19:55 -0300

    red - CreateSceneRepository_OkTest

---
+++ RayTracingApp/Test/MemoryRepositoryTest/SceneRepositoryTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Models;
+
+
+namespace Test.MemoryRepositoryTest
+{
+    [TestClass]
+    public class SceneRepositoryTest
+    {
+        [TestMethod]
+        public void CreateSceneRepository_OkTest()
+        {
+            SceneRepository _sceneRepository = new SceneRepository();
+        }
+    }
+}
