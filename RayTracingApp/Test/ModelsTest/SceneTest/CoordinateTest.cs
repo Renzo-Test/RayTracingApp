c5c0ccb93688671eeef1adcf010a4bcefab867ac
MateoGiraz <mateogiraz27@gmail.com>
2023-04-18 23:50:32 -0300

    red - CreateCoordinate_OkTest

---
+++ RayTracingApp/Test/ModelsTest/SceneTest/CoordinateTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Models;
+
+
+namespace Test.ModelsTest
+{
+    [TestClass]
+    public class CoordinateTest
+    {
+        [TestMethod]
+        public void CreateCoordinate_OkTest()
+        {
+            Coordinate newCoordinate = new Coordinate();
+        }
+    }
+}
