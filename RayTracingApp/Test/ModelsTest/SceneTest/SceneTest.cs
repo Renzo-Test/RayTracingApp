4978319806acf28a94202090691c548755abe308
MateoGiraz <mateogiraz27@gmail.com>
2023-04-18 23:45:41 -0300

    refactor - SceneTest

--- RayTracingApp/Test/ModelsTest/SceneTest/SceneTest.cs
+++ RayTracingApp/Test/ModelsTest/SceneTest/SceneTest.cs
@@ -8,6 +8,8 @@ namespace Test.ModelsTest
 
     public class SceneTest
     {
+        private Scene _scene;
+
         [TestMethod]
         public void CreateScene_OkTest()
         {
@@ -17,55 +19,55 @@ namespace Test.ModelsTest
         [TestMethod]
         public void SetOwner_OkTest()
         {
-            Scene newScene = new Scene()
+            _scene = new Scene()
             {
                 Owner = "ownerName"
             };
-            Assert.AreEqual("ownerName", newScene.Owner);
+            Assert.AreEqual("ownerName", _scene.Owner);
         }
 
         [TestMethod]
         public void SetName_OkTest()
         {
-            Scene newScene = new Scene()
+            _scene = new Scene()
             {
                 Name = "modelName"
             };
-            Assert.AreEqual("modelName", newScene.Name);
+            Assert.AreEqual("modelName", _scene.Name);
         }
 
         [TestMethod]
         public void CanGetRegisterTime_OkTest()
         {
-            Scene newScene = new Scene();
+            _scene = new Scene();
             String today = DateTime.Now.ToString("hh:mm:ss - yyyy/MM/dd");
-            Assert.AreEqual(today, newScene.RegisterTime);
+            Assert.AreEqual(today, _scene.RegisterTime);
         }
 
         [TestMethod]
         public void CanGetLastModificationDate_OkTest()
         {
-            Scene newScene = new Scene();
+            _scene = new Scene();
             String expected = "unmodified";
-            Assert.AreEqual(expected, newScene.LastModificationDate);
+            Assert.AreEqual(expected, _scene.LastModificationDate);
         }
 
         [TestMethod]
         public void CanGetLastRenderDate_OkTest()
         {
-            Scene newScene = new Scene();
+            _scene = new Scene();
             String expected = "unrendered";
-            Assert.AreEqual(expected, newScene.LastRenderDate);
+            Assert.AreEqual(expected, _scene.LastRenderDate);
         }
 
         [TestMethod]
         public void SetFov_OkTest()
         {
-            Scene newScene = new Scene()
+            _scene = new Scene()
             {
                 Fov  = 100,
             };
-            Assert.AreEqual(100, newScene.Fov);
+            Assert.AreEqual(100, _scene.Fov);
         }
 
 
