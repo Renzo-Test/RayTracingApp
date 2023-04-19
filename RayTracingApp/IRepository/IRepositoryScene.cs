78f1b3137c2a6a9405a14279b52491bfcfe68265
MateoGiraz <mateogiraz27@gmail.com>
2023-04-19 15:16:44 -0300

    create SceneRepository interface

---
+++ RayTracingApp/IRepository/IRepositoryScene.cs
@@ -0,0 +1,16 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRepository
+{
+    public interface IRepositoryScene
+    {
+        List<Scene> GetScenesByClient(string username);
+        void AddScene(Scene scene);
+        void RemoveScene(Scene scene);
+    }
+}
