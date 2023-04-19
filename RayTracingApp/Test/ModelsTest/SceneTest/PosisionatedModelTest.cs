37c085f225d11bce00c71de493bf3b31bdd94f3c
MateoGiraz <mateogiraz27@gmail.com>
2023-04-19 00:21:05 -0300

    red - CreatePosisionatedModel_OkTest

---
+++ RayTracingApp/Test/ModelsTest/SceneTest/PosisionatedModelTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Models;
+
+
+namespace Test.ModelsTest
+{
+    [TestClass]
+    public class PosisionatedModelTest
+    {
+        [TestMethod]
+        public void CreatePosisionatedModel_OkTest()
+        {
+            PosisionatedModel posisionatedModel = new PosisionatedModel();
+        }
+    }
+}
