d7f8846d9b25bb5327045366819ae10e0d239b65
Renzo-Test <renzotestorelli27@gmail.com>
2023-04-19 20:39:12 -0300

    red - AddScene_EndWithSpace

--- RayTracingApp/Test/ControllerTest/SceneControllerTest.cs
+++ RayTracingApp/Test/ControllerTest/SceneControllerTest.cs
@@ -58,5 +58,17 @@ namespace Test.ControllerTest
 
             _sceneController.AddScene(newScene, "owneraName");
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidSpacePosition))]
+        public void AddScene_EndWithSpace_FailTest()
+        {
+            Scene newScene = new Scene()
+            {
+                Name = "sceneName "
+            };
+
+            _sceneController.AddScene(newScene, "owneraName");
+        }
     }
 }
