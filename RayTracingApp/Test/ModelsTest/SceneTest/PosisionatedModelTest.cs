ea70acf1627802ea378430eda45ae6a61c397147
MateoGiraz <mateogiraz27@gmail.com>
2023-04-19 00:23:29 -0300

    red - SetPosition_OkTest

--- RayTracingApp/Test/ModelsTest/SceneTest/PosisionatedModelTest.cs
+++ RayTracingApp/Test/ModelsTest/SceneTest/PosisionatedModelTest.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using Models;
+using System.Reflection;
 
 namespace Test.ModelsTest
 {
@@ -11,6 +12,27 @@ namespace Test.ModelsTest
         public void CreatePosisionatedModel_OkTest()
         {
             PosisionatedModel posisionatedModel = new PosisionatedModel();
+        }
+
+        [TestMethod]
+        public void SetPosition_OkTest()
+        {
+            Coordinate newCoordinate = new Coordinate()
+            {
+                X = 10,
+                Y = 20,
+                Z = 30,
+            };
+
+            PosisionatedModel posisionatedModel = new PosisionatedModel()
+            {
+                Position = newCoordinate,
+            };
+
+            foreach (PropertyInfo property in newCoordinate.GetType().GetProperties())
+            {
+                Assert.AreEqual(property.GetValue(newCoordinate), property.GetValue(posisionatedModel.Position));
+            }
         }
     }
 }
