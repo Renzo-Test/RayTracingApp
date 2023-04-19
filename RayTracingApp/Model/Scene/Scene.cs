d91f4d01148e47cd345861a221085a8739959f6f
MateoGiraz <mateogiraz27@gmail.com>
2023-04-18 23:41:16 -0300

    green - CanGetLastRenderDate_OkTest

--- RayTracingApp/Model/Scene/Scene.cs
+++ RayTracingApp/Model/Scene/Scene.cs
@@ -12,5 +12,6 @@ namespace Models
         public string Name { get; set; }
         public string RegisterTime { get; set; } = DateTime.Now.ToString("hh:mm:ss - yyyy/MM/dd");
         public string LastModificationDate { get; set; } = "unmodified";
+        public string LastRenderDate { get; set; } = "unrendered";
     }
 }
