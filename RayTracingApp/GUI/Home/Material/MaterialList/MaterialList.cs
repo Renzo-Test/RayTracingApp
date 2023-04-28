6548df20e9aef1ed7f8962b39cdc694d8faed00f
MateoGiraz <mateogiraz27@gmail.com>
2023-04-28 17:03:18 -0300

    refactor - MaterialList

--- RayTracingApp/GUI/Home/Material/MaterialList/MaterialList.cs
+++ RayTracingApp/GUI/Home/Material/MaterialList/MaterialList.cs
@@ -23,34 +23,18 @@ namespace GUI
         private Client _currentClient;
         public MaterialList(MaterialHome materialHome, MainController mainController, Client currentClient)
         {
-            _mainController = mainController;
-            _materialController = mainController.MaterialController;
-
             _currentClient = currentClient;
             _materialHome = materialHome;
             
+            InitializeControllers(mainController);
+
             InitializeComponent();
-        }
-
-
-        private void MaterialList_Paint(object sender, PaintEventArgs e)
-        {
-            PopulateItems();
         }
 
         public void PopulateItems()
         {
 
-            List<Material> materials;
-
-            try
-            {
-                materials = _materialController.ListMaterials(_currentClient.Username);
-            }
-            catch (NotFoundMaterialException)
-            {
-                return;
-            }
+            List<Material> materials = _materialController.ListMaterials(_currentClient.Username);
 
             flyMaterialList.Controls.Clear();
 
@@ -60,6 +44,16 @@ namespace GUI
                 flyMaterialList.Controls.Add(item);
             }
 
+        }
+        private void InitializeControllers(MainController mainController)
+        {
+            _mainController = mainController;
+            _materialController = mainController.MaterialController;
+        }
+
+        private void MaterialList_Paint(object sender, PaintEventArgs e)
+        {
+            PopulateItems();
         }
 
         private void picAddMaterial_Click(object sender, EventArgs e)
