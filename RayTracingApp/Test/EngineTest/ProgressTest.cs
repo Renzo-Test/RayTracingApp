fd42401988d41ab75461b2b092973a131c9ebad1
Renzo-Test <renzotestorelli27@gmail.com>
2023-05-05 18:50:39 -0300

    refactor

--- RayTracingApp/Test/EngineTest/ProgressTest.cs
+++ RayTracingApp/Test/EngineTest/ProgressTest.cs
@@ -7,6 +7,14 @@ namespace Test.EngineTest
     [TestClass]
     public class ProgressTest
     {
+        private Progress _progress;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _progress = new Progress();
+        }
+
         [TestMethod]
         public void CreateProgress_OkTest()
         {
@@ -16,56 +24,56 @@ namespace Test.EngineTest
         [TestMethod]
         public void SetLinesCount_OkTest()
         {
-            Progress progress = new Progress();
-            progress.LinesCount = 0;
+            Progress _progress = new Progress();
+            _progress.LinesCount = 0;
 
-            Assert.AreEqual(0, progress.LinesCount);
+            Assert.AreEqual(0, _progress.LinesCount);
         }
 
         [TestMethod]
         public void SetLinesCount_uint_OkTest()
         {
-            Progress progress = new Progress();
-            progress.LinesCount = 2147483648;
+            Progress _progress = new Progress();
+            _progress.LinesCount = 2147483648;
 
-            Assert.AreEqual(2147483648, progress.LinesCount);
+            Assert.AreEqual(2147483648, _progress.LinesCount);
         }
 
         [TestMethod]
         public void SetLinesCount_long_OkTest()
         {
-            Progress progress = new Progress();
-            progress.LinesCount = 4294967296;
+            Progress _progress = new Progress();
+            _progress.LinesCount = 4294967296;
 
-            Assert.AreEqual(4294967296, progress.LinesCount);
+            Assert.AreEqual(4294967296, _progress.LinesCount);
         }
 
         [TestMethod]
         public void Count_OkTest()
         {
-            Progress progress = new Progress();
-            
+            Progress _progress = new Progress();
+
             int linesToCount = 5000;
             for (int i = 0; i < linesToCount; i++)
             {
-                progress.Count();
+                _progress.Count();
             }
 
-            Assert.AreEqual(5000, progress.LinesCount);
+            Assert.AreEqual(5000, _progress.LinesCount);
         }
 
         [TestMethod]
         public void Count_otherValue_OkTest()
         {
-            Progress progress = new Progress();
+            Progress _progress = new Progress();
 
             int linesToCount = 999999;
             for (int i = 0; i < linesToCount; i++)
             {
-                progress.Count();
+                _progress.Count();
             }
 
-            Assert.AreEqual(999999, progress.LinesCount);
+            Assert.AreEqual(999999, _progress.LinesCount);
         }
     }
 }
