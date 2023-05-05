d269779d90105d258c0f7d83c7ffd9230b052842
Renzo-Test <renzotestorelli27@gmail.com>
2023-05-05 13:51:29 -0300

    Added Asserts

--- RayTracingApp/Test/EngineTest/VectorTest.cs
+++ RayTracingApp/Test/EngineTest/VectorTest.cs
@@ -25,36 +25,42 @@ namespace Test.EngineTest
         public void SetX_int_OkTest()
         {
             _vector.X = 1;
+            Assert.AreEqual(1, _vector.X);
         }
 
         [TestMethod]
         public void SetX_double_OkTest()
         {
             _vector.X = 1.5;
+            Assert.AreEqual(1.5, _vector.X);
         }
 
         [TestMethod]
         public void SetY_int_OkTest()
         {
             _vector.Y = 1;
+            Assert.AreEqual(1, _vector.Y);
         }
 
         [TestMethod]
         public void SetY_double_OkTest()
         {
             _vector.Y = 1.5;
+            Assert.AreEqual(1.5, _vector.Y);
         }
 
         [TestMethod]
         public void SetZ_int_OkTest()
         {
             _vector.Z = 1;
+            Assert.AreEqual(1, _vector.Z);
         }
 
         [TestMethod]
         public void SetZ_double_OkTest()
         {
             _vector.Z = 1.5;
+            Assert.AreEqual(1.5, _vector.Z);
         }
 
         [TestMethod]
@@ -62,6 +68,7 @@ namespace Test.EngineTest
         {
             Vector vectorToAdd = new Vector();
             _vector.Add(vectorToAdd);
+            Assert.AreEqual(2, _vector.X);
         }
     }
 }
