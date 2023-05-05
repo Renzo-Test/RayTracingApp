59b61caaa2eddab49de5c6c82f4357a2a627b15c
Renzo-Test <renzotestorelli27@gmail.com>
2023-05-05 13:33:24 -0300

    red - CreateVector

---
+++ RayTracingApp/Test/EngineTest/VectorTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Test.EngineTest
+{
+    [TestClass]
+    public class VectorTest
+    {
+        [TestMethod]
+        public void CreateVector_OkTest()
+        {
+            Vector vector = new Vector();
+        }
+    }
+}
