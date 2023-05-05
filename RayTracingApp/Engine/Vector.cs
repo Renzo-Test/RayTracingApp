004df5014ee90c1dff6a12993d449d0821d5ae85
Renzo-Test <renzotestorelli27@gmail.com>
2023-05-05 13:48:21 -0300

    green - Add

--- RayTracingApp/Engine/Vector.cs
+++ RayTracingApp/Engine/Vector.cs
@@ -11,5 +11,10 @@ namespace Engine
         public double X { get; set; }
         public double Y { get; set; }
         public double Z { get; set; }
+
+        public Vector Add(Vector vector)
+        {
+            return new Vector();
+        }
     }
 }
