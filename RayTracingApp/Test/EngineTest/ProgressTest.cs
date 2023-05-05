96288a297a88dad20dcbc8c7be29c61823b3d572
Renzo-Test <renzotestorelli27@gmail.com>
2023-05-05 18:35:26 -0300

    red - SetLinesCount

---
+++ RayTracingApp/Test/EngineTest/ProgressTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Test.EngineTest
+{
+    [TestClass]
+    public class ProgressTest
+    {
+        [TestMethod]
+        public void CreateProgress()
+        {
+            Progress progress = new System.Progress();
+        }
+    }
+}
