f5cb53940c9efc711b4159767079cf22b4f8d26d
Renzo-Test <renzotestorelli27@gmail.com>
2023-05-07 20:14:42 -0300

    red - CreatePrinter

---
+++ RayTracingApp/Test/EngineTest/PrinterTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Test.EngineTest
+{
+    [TestClass]
+    public class PrinterTest
+    {
+        [TestMethod]
+        public void CreatePrinter_OkTest()
+        {
+            Printer printer = new Printer();
+        }
+    }
+}
