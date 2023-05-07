e1a2f35c3efac031a7ba52a64523ff7def52fb18
Renzo-Test <renzotestorelli27@gmail.com>
2023-05-07 01:00:54 -0300

    created specific exception - SetResolution_LowerThanMin

---
+++ RayTracingApp/Engine/RenderProperties/Exceptions/InputLowerThanZero.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.RenderProperties.Exceptions
+{
+	internal class InputLowerThanZero : InvalidRenderPropertiesInputException
+	{
+		public InputLowerThanZero (string message) : base (message) { }
+	}
+}
