4cbc2e9b10fa9857a65bb15269d7ae5f9fd72d94
Renzo-Test <renzotestorelli27@gmail.com>
2023-05-07 00:58:00 -0300

    created base exception - SetResolution_LowerThanMin

---
+++ RayTracingApp/Engine/RenderProperties/Exceptions/InvalidRenderPropertiesException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.RenderProperties.Exceptions
+{
+	public class InvalidRenderPropertiesInputException : Exception
+	{
+		public InvalidRenderPropertiesInputException(string message) : base(message) { }
+	}
+}
