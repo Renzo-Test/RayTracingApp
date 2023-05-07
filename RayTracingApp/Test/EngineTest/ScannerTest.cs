d59afb927224cbf87c7c2104165c4a6cec2c3e5b
MateoGiraz <mateogiraz27@gmail.com>
2023-05-07 19:41:21 -0300

    red - CreateScanner_OkTest

---
+++ RayTracingApp/Test/EngineTest/ScannerTest.cs
@@ -0,0 +1,19 @@
+﻿using Engine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Test.EngineTest
+{
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    public class ScannerTest
+    {
+        [TestMethod]
+        public void CreateScanner_OkTest()
+        {
+            Scanner scanner = new Scanner();
+        }
+
+    }
+}
