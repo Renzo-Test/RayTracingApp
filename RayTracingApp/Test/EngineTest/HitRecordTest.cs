a9d04fa69c757d80dc05b2def20e93ff76c6a7dd
Renzo-Test <renzotestorelli27@gmail.com>
2023-05-06 21:19:47 -0300

    red - CreateHitRecord

---
+++ RayTracingApp/Test/EngineTest/HitRecordTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Test.EngineTest
+{
+	[TestClass]
+	public class HitRecordTest
+	{
+		[TestMethod]
+		public void CreateHitRecord_OkTest()
+		{
+			HitRecord hitRecord = new HitRecord();
+		}
+	}
+}
