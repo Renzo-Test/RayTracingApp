9183607fe682ea648d4dee307315088d19f48201
Renzo-Test <renzotestorelli27@gmail.com>
2023-05-06 21:29:48 -0300

    red - SetT_double

--- RayTracingApp/Test/EngineTest/HitRecordTest.cs
+++ RayTracingApp/Test/EngineTest/HitRecordTest.cs
@@ -21,5 +21,14 @@ namespace Test.EngineTest
 
 			Assert.AreEqual(1, hitRecord.T);
 		}
+
+		[TestMethod]
+		public void SetT_double_OkTest()
+		{
+			HitRecord hitRecord = new HitRecord();
+			hitRecord.T = 1.5;
+
+			Assert.AreEqual(1.5, hitRecord.T);
+		}
 	}
 }
