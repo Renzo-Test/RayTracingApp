47299401e306824fe7a97f4c08797109f3a19689
MateoGiraz <mateogiraz27@gmail.com>
2023-05-08 20:29:22 -0300

    red - CanGetRay_OkTest

--- RayTracingApp/Test/EngineTest/CameraTest.cs
+++ RayTracingApp/Test/EngineTest/CameraTest.cs
@@ -22,5 +22,40 @@ namespace Test.EngineTest
 
             Camera camera = new Camera(newVector, newVector, newVector, fieldOfView, aspectRatio);
         }
+
+        [TestMethod]
+        public void CanGetRay_OkTest()
+        {
+            Vector newVector = new Vector()
+            {
+                X = 10,
+                Y = 20,
+                Z = 30
+            };
+
+            int fieldOfView = 10;
+            double aspectRatio = 10;
+
+            Camera camera = new Camera(newVector, newVector, newVector, fieldOfView, aspectRatio);
+
+            Vector horizontalPosition = newVector.Multiply(1);
+            Vector verticalPosition = newVector.Multiply(2);
+
+            Vector pointPosition = newVector.Add(horizontalPosition.Add(verticalPosition));
+
+            Ray newRay = new Ray()
+            {
+                Origin = newVector,
+                Direction = pointPosition.Substract(newVector)
+            };
+
+            Ray cameraRay = camera.GetRay(1, 2);
+
+            Assert.AreEqual(newRay.Origin, cameraRay.Origin);
+            Assert.AreEqual(newRay.Direction, cameraRay.Direction);
+
+        }
+
+
     }
 }
