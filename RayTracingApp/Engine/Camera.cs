80074f05f8132110b4e1e5fb6609871de7750be5
MateoGiraz <mateogiraz27@gmail.com>
2023-05-08 20:25:05 -0300

    green - CanCreateCamera_OkTest

---
+++ RayTracingApp/Engine/Camera.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class Camera
+    {
+        private Vector VectorLowerLeftCorner { get; set; }
+        private Vector VectorHorizontal { get; set; }
+        private Vector VectorVertical { get; set; }
+        private Vector Origin { get; set; }
+
+        public Camera(Vector vectorLookFrom, Vector vectorLookAt, Vector vectorUp, int fieldOfView, double aspectRatio)
+        {
+
+        }
+    }
+}
