65e1e03564bd683aa12162272622a3ee00c467b8
MateoGiraz <mateogiraz27@gmail.com>
2023-05-08 20:23:17 -0300

    red - CanCreateCamera_OkTest

---
+++ RayTracingApp/Test/EngineTest/CameraTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Test.EngineTest
+{
+    [TestClass]
+    public class CameraTest
+    {
+        [TestMethod]
+        public void CanCreateCamera_OkTest()
+        {
+            Vector newVector = new Vector()
+            {
+                X = 10,
+                Y = 20,
+                Z = 30
+            };
+
+            int fieldOfView = 10;
+            double aspectRatio = 10;
+
+            Camera camera = new Camera(newVector, newVector, newVector, fieldOfView, aspectRatio);
+        }
+    }
+}
