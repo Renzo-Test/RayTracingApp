00bc14365e4582e55eebd469c9812d2a5a566006
MateoGiraz <mateogiraz27@gmail.com>
2023-05-08 20:37:27 -0300

    refactor - CanGetRay_OkTest

--- RayTracingApp/Engine/Camera.cs
+++ RayTracingApp/Engine/Camera.cs
@@ -18,10 +18,13 @@ namespace Engine
             double Theta = fieldOfView * Math.PI / 180;
             double HeightHalf = Math.Tan(Theta / 2);
             double WidthHalf = aspectRatio * HeightHalf;
+            
             Origin = vectorLookFrom;
+
             Vector VectorW = vectorLookFrom.Substract(vectorLookAt).GetUnit();
             Vector VectorU = vectorUp.Cross(VectorW).GetUnit();
             Vector VectorV = VectorW.Cross(VectorU);
+            
             VectorLowerLeftCorner = Origin.Substract(VectorU.Multiply(WidthHalf)).Substract(VectorV.Multiply(HeightHalf)).Substract(VectorW);
             VectorHorizontal = VectorU.Multiply(2 * WidthHalf);
             VectorVertical = VectorV.Multiply(2 * HeightHalf);
