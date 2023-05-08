37da1d16229658b46dec985061e69a82df395b56
Renzo-Test <renzotestorelli27@gmail.com>
2023-05-08 14:26:37 -0300

    red - Progress

--- RayTracingApp/Test/EngineTest/PrinterTest.cs
+++ RayTracingApp/Test/EngineTest/PrinterTest.cs
@@ -58,5 +58,29 @@ namespace Test.EngineTest
 			string expected = $"P3\n1 1\n255\n100 100 100\n";
 			Assert.AreEqual(expected, printer.Save(pixels, properties, null));
 		}
+
+        [TestMethod]
+        public void Progress_OkTest()
+        {
+			RenderProperties properties = new RenderProperties()
+			{
+				ResolutionX = 1,
+				ResolutionY = 1,
+
+			};
+			Color color = new Color()
+			{
+				Red = 100,
+				Green = 100,
+				Blue = 100,
+			};
+			pixels.Add(new List<Color> { color });
+            Progress progress = new Progress()
+            {
+                ExpectedLines = 1,
+            };
+
+            Assert.AreEqual(100, progress.Calculate());
+		}
 	}
 }
