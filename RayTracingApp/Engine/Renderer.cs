39487a1d02efce28fc6b8d22aa4217908e35b616
Renzo-Test <renzotestorelli27@gmail.com>
2023-05-08 22:58:58 -0300

    green - CreateRenderer

--- RayTracingApp/Engine/Renderer.cs
+++ RayTracingApp/Engine/Renderer.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace Renderer
+namespace Engine
 {
-	internal class Engine
+	public class Renderer
 	{
 	}
 }
