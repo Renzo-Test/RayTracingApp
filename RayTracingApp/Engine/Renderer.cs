2fe2cc8c4e5a2891aa818cc62192df46dbf53642
Renzo-Test <renzotestorelli27@gmail.com>
2023-05-08 23:03:03 -0300

    Renderer properties

--- RayTracingApp/Engine/Renderer.cs
+++ RayTracingApp/Engine/Renderer.cs
@@ -9,5 +9,12 @@ namespace Engine
 {
 	public class Renderer
 	{
+		private Printer _printer;
+		private List<List<Vector>> _pixels;
+		private Progress _progress;
+
+		private Camera _camera;
+
+		private static readonly ThreadLocal<Random> random = new ThreadLocal<Random>(() => new Random());
 	}
 }
