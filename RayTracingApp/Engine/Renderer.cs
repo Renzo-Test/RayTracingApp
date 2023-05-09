70407ab0960afb0154e9e5d6ce3abc7c20ab4ef0
Renzo-Test <renzotestorelli27@gmail.com>
2023-05-09 00:11:10 -0300

    Render

--- RayTracingApp/Engine/Renderer.cs
+++ RayTracingApp/Engine/Renderer.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -16,5 +18,56 @@ namespace Engine
 		private Camera _camera;
 
 		private static readonly ThreadLocal<Random> random = new ThreadLocal<Random>(() => new Random());
+
+		public string Render(Scene scene, RenderProperties properties)
+		{
+			_progress = new Progress();
+			_pixels = new List<List<Vector>>();
+			_printer = new Printer();
+
+			_progress.ExpectedLines = (properties.ResolutionY * properties.ResolutionX * properties.SamplesPerPixel) + properties.ResolutionY;
+
+
+			double AspectRatio = properties.AspectRatio;
+
+			//created before iterate
+			for (int i = 0; i < properties.ResolutionY; i++)
+			{
+				_pixels.Add(new List<Vector>());
+			}
+
+			int row = properties.ResolutionY - 1;
+			Parallel.For(0, row + 1, index =>
+			{
+				int derivatedIndex = row - index;
+				for (int column = 0; column < properties.ResolutionX; column++)
+				{
+					Vector vector = new Vector() //no pongo constructor por clean code
+					{
+						X = 0,
+						Y = 0,
+						Z = 0
+					};
+
+					for (int sample = 0; sample < properties.SamplesPerPixel; sample++)
+					{
+						double fstRnd = random.Value.NextDouble();
+						double sndRnd = random.Value.NextDouble();
+
+						double u = (column + fstRnd) / properties.ResolutionX;
+						double v = (derivatedIndex + sndRnd) / properties.ResolutionY;
+
+						var ray = _camera.GetRay(u, v);
+						vector.AddFrom(ShootRay(ray, properties.MaxDepth));
+						_progress.Count(); ;
+					}
+
+					vector = vector.Divide(properties.SamplesPerPixel);
+					Vector color = new Vector();
+					SavePixel(derivatedIndex, column, color);
+				}
+			});
+			return _printer.Save(_pixels, properties, ref _progress);
+		}
 	}
 }
