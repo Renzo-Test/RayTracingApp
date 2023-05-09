761a0f84793e860bc54a5f9ca4b8e58b6fcc6ae4
Renzo-Test <renzotestorelli27@gmail.com>
2023-05-09 19:15:01 -0300

    gren - DefaultLookFromX

--- RayTracingApp/Domain/Client/Client.cs
+++ RayTracingApp/Domain/Client/Client.cs
@@ -50,7 +50,7 @@ namespace Domain
 		}
 		public String RegisterDate { get; } = DateTime.Today.ToString("dd/MM/yyyy");
 		public int DefaultFov = 30;
-		public Vector DefaultLookFrom { get; set; }
+		public Vector DefaultLookFrom { get; set; } = new Vector() { X = 0 };
 		public Vector DefaultLookAt { get; set; }
 
 		private static void RunUsernameConditions(string username)
