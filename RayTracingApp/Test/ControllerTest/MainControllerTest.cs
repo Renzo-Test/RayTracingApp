832173e292ed622894e9fe6ad1bbf50fc6215c7a
Renzo-Test <renzotestorelli27@gmail.com>
2023-05-11 16:55:01 -0300

    MainController - 100

---
+++ RayTracingApp/Test/ControllerTest/MainControllerTest.cs
@@ -0,0 +1,16 @@
+﻿using Controller;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Test.ControllerTest
+{
+	[TestClass]
+	public class MainControllerTest
+	{
+		[TestMethod]
+		public void CreateMainController_OkTest()
+		{
+			MainController controller = new MainController();
+		}
+	}
+}
