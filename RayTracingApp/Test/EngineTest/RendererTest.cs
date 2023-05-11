ac9c9fdc53bfa5f03788f181a5cef7bc72a72ba0
Renzo-Test <renzotestorelli27@gmail.com>
2023-05-11 14:29:26 -0300

    Render test

--- RayTracingApp/Test/EngineTest/RendererTest.cs
+++ RayTracingApp/Test/EngineTest/RendererTest.cs
@@ -1,4 +1,5 @@
-﻿using Engine;
+﻿using Domain;
+using Engine;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
@@ -11,6 +12,38 @@ namespace Test.EngineTest
 		public void CreateRenderer_OkTest()
 		{
 			Renderer renderer = new Renderer();
+		}
+
+		[TestMethod]
+		public void Render_OkTest()
+		{
+			Renderer renderer = new Renderer();
+			Scene scene = new Scene();
+			RenderProperties properties = new RenderProperties()
+			{
+				ResolutionX = 3,
+			};
+			string render = renderer.Render(scene, properties, null);
+
+			int propertiesChars = 11;
+			int pixelsChars = 12 * (properties.ResolutionX * properties.ResolutionY);
+			Assert.AreEqual(propertiesChars + pixelsChars, render.Length);
+		}
+
+		[TestMethod]
+		public void Render_BiggerResolution_OkTest()
+		{
+			Renderer renderer = new Renderer();
+			Scene scene = new Scene();
+			RenderProperties properties = new RenderProperties()
+			{
+				ResolutionX = 100,
+			};
+			string render = renderer.Render(scene, properties, null);
+
+			int propertiesChars = 11;
+			int pixelsChars = 12 * (properties.ResolutionX * properties.ResolutionY);
+			Assert.AreEqual(propertiesChars + pixelsChars, render.Length);
 		}
 	}
 }
