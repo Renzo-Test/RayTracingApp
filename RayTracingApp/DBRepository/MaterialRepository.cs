cb14be29c146fdc3246649af34c1f9136ecc98ac
MateoGiraz <mateogiraz27@gmail.com>
2023-05-28 21:48:02 -0300

    data-persistence AddMaterial

---
+++ RayTracingApp/DBRepository/MaterialRepository.cs
@@ -0,0 +1,38 @@
+﻿using Domain;
+using IRepository;
+using MemoryRepository.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBRepository
+{
+    public class MaterialRepository : IRepositoryMaterial
+    {
+        private const string NotFoundMaterialMessage = "Material was not found or already deleted";
+
+        public string DBName { get; set; } = "RayTracingAppDB";
+
+
+        public List<Material> GetMaterialsByClient(string username)
+        {
+            return new List<Material>();
+        }
+
+        public void AddMaterial(Material newMaterial)
+        {
+            using (var context = new AppContext(DBName))
+            {
+                context.Materials.Add(newMaterial);
+                context.SaveChanges();
+            }
+
+        }
+
+        public void RemoveMaterial(Material material)
+        {
+        }
+    }
+}
