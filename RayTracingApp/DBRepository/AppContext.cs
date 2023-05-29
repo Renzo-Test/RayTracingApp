726f1355a1e1336c6e23d7e7c4c566219753db88
MateoGiraz <mateogiraz27@gmail.com>
2023-05-28 21:46:40 -0300

    data-persistence - added materials dbset

--- RayTracingApp/DBRepository/AppContext.cs
+++ RayTracingApp/DBRepository/AppContext.cs
@@ -11,6 +11,7 @@ namespace DBRepository
     public class AppContext : DbContext
     {
         public DbSet<Figure> Figures { get; set; }
+        public DbSet<Material> Materials { get; set; }
         public AppContext(string dbName) : base(dbName) { }
 
         public void ClearDBTable(string table) {
