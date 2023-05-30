da3fbbb7287f0df8293cd7062023a704bbc7016c
Santiago <santicapoactualizado2@gmail.com>
2023-05-30 17:03:53 -0300

    SceneRepository-AddScene

--- RayTracingApp/DBRepository/SceneRepository.cs
+++ RayTracingApp/DBRepository/SceneRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Domain;
+using MemoryRepository.Exceptions;
+using IRepository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,27 @@ using System.Threading.Tasks;
 
 namespace DBRepository
 {
-    internal class Class1
+    public class SceneRepository : IRepositoryScene
     {
+        private const string NotFoundSceneMessage = "Scene was not found or already deleted";
+        public string DBName { get; set; } = "RayTracingAppDB";
+        public void AddScene(Scene scene)
+        {
+            using (var context = new AppContext(DBName)) 
+            {
+                context.Scenes.Add(scene);
+                context.SaveChanges();
+            }
+        }
+
+        public List<Scene> GetScenesByClient(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveScene(Scene scene)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
