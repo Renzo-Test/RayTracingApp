bc69ff77fff692cf502ec63c1ed104091bcc45db
MateoGiraz <mateogiraz27@gmail.com>
2023-06-01 11:59:26 -0300

    red - CanCreateJPGExporter_OkTest

---
+++ RayTracingApp/Test/EngineTest/ExporterTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Engine.Exporter;
+
+namespace Test.EngineTest
+{
+    [TestClass]
+    public class ExporterTest
+    {
+        [TestMethod]
+        public void CanCreateJPGExporter_OkTest()
+        {
+            IExporter exporter = new JPGExporter();
+        }
+
+    }
+}
