e41d4db74904c28167beb087ad6536868705dcb1
MateoGiraz <mateogiraz27@gmail.com>
2023-06-01 13:22:37 -0300

    green - CanCreatePNGExporter_OkTest

---
+++ RayTracingApp/Engine/Exporter/PNGExporter.cs
@@ -0,0 +1,20 @@
+﻿using Engine.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Exporter
+{
+    public class PNGExporter : IExporter
+    {
+        public void Export(string path, Image img)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
