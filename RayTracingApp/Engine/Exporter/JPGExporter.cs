d09c5a450073fb948183e4fb687985dd6bcff277
MateoGiraz <mateogiraz27@gmail.com>
2023-06-01 12:02:21 -0300

    green - CanCreateJPGExporter_OkTest

---
+++ RayTracingApp/Engine/Exporter/JPGExporter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Exporter
+{
+    public class JPGExporter : IExporter
+    {
+        public void Export(string path, Image img)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
