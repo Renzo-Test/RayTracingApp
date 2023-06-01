ffe9af8e41a1d707c02c98cabf390a7d9a9354fa
MateoGiraz <mateogiraz27@gmail.com>
2023-06-01 13:31:55 -0300

    green - CanCreatePPMExporter_OkTest

---
+++ RayTracingApp/Engine/Exporter/PPMExporter.cs
@@ -0,0 +1,20 @@
+﻿using Engine.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Exporter
+{
+    public class PPMExporter : IExporter
+    {
+        public void Export(string path, Image img)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
