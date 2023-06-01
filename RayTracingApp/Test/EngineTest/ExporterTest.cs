65427dfd0d78072dfedfbc67cc0c7a74468d0477
MateoGiraz <mateogiraz27@gmail.com>
2023-06-01 12:08:58 -0300

    red - CanExport_JPGExporter_OkTest

--- RayTracingApp/Test/EngineTest/ExporterTest.cs
+++ RayTracingApp/Test/EngineTest/ExporterTest.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using Engine.Exporter;
+using System.Drawing;
+using System.IO;
 
 namespace Test.EngineTest
 {
@@ -12,6 +14,16 @@ namespace Test.EngineTest
         {
             IExporter exporter = new JPGExporter();
         }
+
+        [TestMethod]
+        public void CanExport_JPGExporter_OkTest()
+        {
+            IExporter exporter = new JPGExporter();
+            exporter.Export(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures) + "image.jpg", new Bitmap("test"));
+
+            Assert.IsTrue(File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures) + "image.jpg"));
+        }
+
 
     }
 }
