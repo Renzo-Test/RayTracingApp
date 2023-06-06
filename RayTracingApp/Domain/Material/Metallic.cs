4722c58cdeaed95a0f6b98db2c7176d7a9181e1e
SanJingSung <santicapoactualizado2@gmail.com>
2023-06-06 09:24:03 -0300

    green-SetBlur_InvalidBlur_OkTest

--- RayTracingApp/Domain/Material/Metallic.cs
+++ RayTracingApp/Domain/Material/Metallic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain.Exceptions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,8 +10,30 @@ namespace Domain
     public class Metallic : Material
     {
         private double _blur;
-        public double Blur { get { return _blur; } set { _blur = value; } }
+        public const string NotValidBlurMessage = "Blur value must be greater or equal than 0.0";
+        public double Blur
+        {
+            get { return _blur; }
+            set
+            {
+                try
+                {
+                    isValidBlur(value);
+                    _blur = value;
+                }
+                catch(InvalidMaterialInputException ex)
+                {
+                    throw new InvalidMaterialInputException(ex.Message);
+                }
+            }
+        }
         public Metallic() : base(MaterialEnum.Metallic) { }
-
+        private static void isValidBlur(double value) 
+        {
+            if (value < 0.0) 
+            {
+                throw new InvalidMaterialInputException(NotValidBlurMessage);
+            }
+        }
     }
 }
