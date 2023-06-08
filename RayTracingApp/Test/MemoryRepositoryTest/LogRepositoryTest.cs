563f87d47d1f1a4499f7cf6701bb6ed6db3453fc
Santiago <santicapoactualizado2@gmail.com>
2023-06-08 19:49:57 -0300

    red-CanCreateLogRepository_OkTest

---
+++ RayTracingApp/Test/MemoryRepositoryTest/LogRepositoryTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Test.MemoryRepositoryTest
+{
+    [TestClass]
+    public class LogRepositoryTest
+    {
+        [TestCleanup]
+        public void TestCleanUp()
+        {
+            using (var context = new DBRepository.AppContext("RayTracingAppTestDB"))
+            {
+                context.ClearDBTable("Figures");
+            }
+        }
+
+        [TestMethod]
+        public void CanCreateLogRepository_OkTest()
+        {
+            LogRepository newLogRepository = new LogRepository(new);
+        }
+    }
+}
