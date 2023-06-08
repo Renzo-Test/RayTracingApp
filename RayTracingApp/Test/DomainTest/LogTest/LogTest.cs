e6fbb8210b472b047db944fefd3280cdd72a4cdd
Santiago <santicapoactualizado2@gmail.com>
2023-06-08 19:12:11 -0300

    green-CanCreateLog_OkTest

--- RayTracingApp/Test/DomainTest/LogTest/LogTest.cs
+++ RayTracingApp/Test/DomainTest/LogTest/LogTest.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using Domain;
 
 namespace Test.ModelsTest
 {
     [TestClass]
     public class LogTest
     {
+
         [TestMethod]
         public void CanCreateLog_OkTest()
         {
