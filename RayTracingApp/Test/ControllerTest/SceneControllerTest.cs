9a90430989fdb6805e90ed8cc3ed977c3b64ec73
Renzo-Test <renzotestorelli27@gmail.com>
2023-06-12 18:28:38 -0300

    SceneControllerTest - UpdateLastModificationDate fix

--- RayTracingApp/Test/ControllerTest/SceneControllerTest.cs
+++ RayTracingApp/Test/ControllerTest/SceneControllerTest.cs
@@ -178,7 +178,7 @@ namespace Test.ControllerTest
 			};
 
 			_sceneController.UpdateLastModificationDate(newScene);
-			Assert.AreEqual(DateTime.Now.ToString("hh:mm:ss - dd/MM/yyyy"), newScene.LastModificationDate);
+			Assert.AreEqual(DateTime.Now.ToString("HH:mm:ss - dd/MM/yyyy"), newScene.LastModificationDate);
 		}
 
 		[TestMethod]
