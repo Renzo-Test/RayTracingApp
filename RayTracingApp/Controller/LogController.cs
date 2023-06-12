660ff0d60ead846fc6d946e3534934db4cb585b7
Renzo-Test <renzotestorelli27@gmail.com>
2023-06-12 19:17:13 -0300

    general refactor

--- RayTracingApp/Controller/LogController.cs
+++ RayTracingApp/Controller/LogController.cs
@@ -1,80 +1,76 @@
 ﻿using DBRepository;
-using Domain.Exceptions;
 using Domain;
 using IRepository;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Controller
 {
-    public class LogController
-    {
-        private const string DefaultDatabase = "RayTracingAppDB";
-        public IRepositoryLog Repository;
+	public class LogController
+	{
+		private const string DefaultDatabase = "RayTracingAppDB";
+		public IRepositoryLog Repository;
 
-        public LogController(string dbName = DefaultDatabase)
-        {
-            Repository = new LogRepository()
-            {
-                DBName = dbName,
-            };
-        }
-        public void AddLog(Log newLog)
-        {
-            Repository.AddLog(newLog);
-        }
-        public List<Log> ListLogs()
-        {
-            return Repository.GetAllLogs();
-        }
-        public string GetUserWithMaxAccumulatedRenderTime()
-        {
-            List<Log> logs = Repository.GetAllLogs();
+		public LogController(string dbName = DefaultDatabase)
+		{
+			Repository = new LogRepository()
+			{
+				DBName = dbName,
+			};
+		}
+		public void AddLog(Log newLog)
+		{
+			Repository.AddLog(newLog);
+		}
+		public List<Log> ListLogs()
+		{
+			return Repository.GetAllLogs();
+		}
+		public string GetUserWithMaxAccumulatedRenderTime()
+		{
+			List<Log> logs = Repository.GetAllLogs();
 
-            var userMaxRenderTime = logs
-                .GroupBy(log => log.Username)
-                .Select(group => new
-                {
-                    Username = group.Key,
-                    AccumulatedRenderTime = group.Sum(log => log.RenderTime)
-                })
-                .OrderByDescending(group => group.AccumulatedRenderTime)
-                .FirstOrDefault();
+			var userMaxRenderTime = logs
+				.GroupBy(log => log.Username)
+				.Select(group => new
+				{
+					Username = group.Key,
+					AccumulatedRenderTime = group.Sum(log => log.RenderTime)
+				})
+				.OrderByDescending(group => group.AccumulatedRenderTime)
+				.FirstOrDefault();
 
-            if (userMaxRenderTime != null)
-            {
-                return userMaxRenderTime.Username;
-            }
+			if (userMaxRenderTime != null)
+			{
+				return userMaxRenderTime.Username;
+			}
 
-            return string.Empty;
-        }
-        public int GetTotalRenderTimeInMinutes()
-        {
-            List<Log> logs = Repository.GetAllLogs();
-            int totalRenderTime = logs.Sum(log => log.RenderTime);
+			return string.Empty;
+		}
+		public int GetTotalRenderTimeInMinutes()
+		{
+			List<Log> logs = Repository.GetAllLogs();
+			int totalRenderTime = logs.Sum(log => log.RenderTime);
 
-            // Convertir de segundos a minutos
-            return totalRenderTime / 60;
-        }
+			// Convertir de segundos a minutos
+			return totalRenderTime / 60;
+		}
 
-        public int GetAverageRenderTimeInSeconds()
-        {
-            List<Log> logs = Repository.GetAllLogs();
-            int totalRenderTime = logs.Sum(log => log.RenderTime);
-            int totalLogs = logs.Count;
+		public int GetAverageRenderTimeInSeconds()
+		{
+			List<Log> logs = Repository.GetAllLogs();
+			int totalRenderTime = logs.Sum(log => log.RenderTime);
+			int totalLogs = logs.Count;
 
-            if (totalLogs > 0)
-            {
-                return totalRenderTime / totalLogs;
-            }
+			if (totalLogs > 0)
+			{
+				return totalRenderTime / totalLogs;
+			}
 
-            return 0;
-        }
+			return 0;
+		}
 
-    }
+	}
 
 }
 
--- RayTracingApp/DBRepository/LogRepository.cs
+++ RayTracingApp/DBRepository/LogRepository.cs
@@ -1,32 +1,29 @@
-﻿using System;
+﻿using Domain;
 using IRepository;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Domain;
 
 namespace DBRepository
 {
-    public class LogRepository : IRepositoryLog
-    {
-        public string DBName { get; set; } = "RayTracingAppDB";
+	public class LogRepository : IRepositoryLog
+	{
+		public string DBName { get; set; } = "RayTracingAppDB";
 
-        public void AddLog(Log log)
-        {
-            using (var context = new AppContext(DBName))
-            {
-                context.Logs.Add(log);
-                context.SaveChanges();
-            }
-        }
+		public void AddLog(Log log)
+		{
+			using (var context = new AppContext(DBName))
+			{
+				context.Logs.Add(log);
+				context.SaveChanges();
+			}
+		}
 
-        public List<Log> GetAllLogs()
-        {
-            using (var context = new AppContext(DBName))
-            {
-                return context.Logs.ToList();
-            }
-        }
-    }
+		public List<Log> GetAllLogs()
+		{
+			using (var context = new AppContext(DBName))
+			{
+				return context.Logs.ToList();
+			}
+		}
+	}
 }
