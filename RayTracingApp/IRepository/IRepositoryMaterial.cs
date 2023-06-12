788aa5930a50aac3f77d28a29cdfef793c13caf6
Renzo-Test <renzotestorelli27@gmail.com>
2023-06-12 20:50:04 -0300

    IRepositoryMaterial - GetMaterialByClient

--- RayTracingApp/IRepository/IRepositoryMaterial.cs
+++ RayTracingApp/IRepository/IRepositoryMaterial.cs
@@ -5,7 +5,7 @@ namespace IRepository
 {
 	public interface IRepositoryMaterial
 	{
-		List<Material> GetMaterialsByClient(string username);
+		List<Material> GetMaterialsByClient(Client client);
 		void AddMaterial(Material material);
 		void RemoveMaterial(Material material);
 		void UpdateMaterialName(Material material, string newName);
