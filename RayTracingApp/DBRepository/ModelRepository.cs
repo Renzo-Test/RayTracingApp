b48e0fe04a812489ea2aa43f6677b9628d76b191
Renzo-Test <renzotestorelli27@gmail.com>
2023-06-12 21:07:00 -0300

    ModelRepository - AddModel

--- RayTracingApp/DBRepository/ModelRepository.cs
+++ RayTracingApp/DBRepository/ModelRepository.cs
@@ -14,16 +14,18 @@ namespace DBRepository
 
 		public string DBName { get; set; } = "RayTracingAppDB";
 
-		public void AddModel(Model newModel)
+		public void AddModel(Model model, Client client)
 		{
 			using (var context = new AppContext(DBName))
 			{
-				Material material = context.Materials.FirstOrDefault(m => m.Id.Equals(newModel.Material.Id));
-				newModel.Material = material;
-				Figure figure = context.Figures.FirstOrDefault(f => f.Id.Equals(newModel.Figure.Id));
-				newModel.Figure = figure;
+				Client modelClient = context.Clients.FirstOrDefault(c => c.Id == client.Id);
+				model.Owner = modelClient;
+				Material material = context.Materials.FirstOrDefault(m => m.Id.Equals(model.Material.Id));
+				model.Material = material;
+				Figure figure = context.Figures.FirstOrDefault(f => f.Id.Equals(model.Figure.Id));
+				model.Figure = figure;
 
-				context.Models.Add(newModel);
+				context.Models.Add(model);
 				context.SaveChanges();
 			}
 		}
