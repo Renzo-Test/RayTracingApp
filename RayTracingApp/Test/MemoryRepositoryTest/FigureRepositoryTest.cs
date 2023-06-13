20d1fa1aca9f80660d648232b6cd99a4a8acdeb6
Renzo-Test <renzotestorelli27@gmail.com>
2023-06-12 22:13:16 -0300

    FigureRepositoryTest - compile

--- RayTracingApp/Test/MemoryRepositoryTest/FigureRepositoryTest.cs
+++ RayTracingApp/Test/MemoryRepositoryTest/FigureRepositoryTest.cs
@@ -13,6 +13,8 @@ namespace Test.MemoryRepositoryTest
 	public class FigureRepositoryTest
 	{
 		private FigureRepository _figureRepository;
+		private Client _owner;
+		private Client _otherOwner;
 
 		[TestInitialize]
 		public void TestInitialize()
@@ -21,6 +23,9 @@ namespace Test.MemoryRepositoryTest
 			{
 				DBName = "RayTracingAppTestDB"
 			};
+
+			_owner = new Client() { Username = "ownerName" };
+			_otherOwner = new Client() { Username = "otherName" };
 		}
 
 		[TestCleanup]
@@ -46,7 +51,7 @@ namespace Test.MemoryRepositoryTest
 				Name = "Test",
 				Owner = _owner
 			};
-			_figureRepository.AddFigure(newFigure);
+			_figureRepository.AddFigure(newFigure, _owner);
 
 			Assert.AreEqual(newFigure.Name, _figureRepository.GetFiguresByClient(_owner)[0].Name);
 			Assert.AreEqual(newFigure.Owner, _figureRepository.GetFiguresByClient(_owner)[0].Owner);
@@ -59,25 +64,27 @@ namespace Test.MemoryRepositoryTest
 			Figure firstFigure = new Sphere()
 			{
 				Name = "FigureOne",
-				Owner = "OwnerOne"
+				Owner = _owner
 			};
-			_figureRepository.AddFigure(firstFigure);
+			_figureRepository.AddFigure(firstFigure, _owner);
 
 			Figure secondFigure = new Sphere()
 			{
 				Name = "FigureTwo",
-				Owner = "OwnerTwo"
+				Owner = _otherOwner
 			};
-			_figureRepository.AddFigure(secondFigure);
+			_figureRepository.AddFigure(secondFigure, _otherOwner);
 
-			Assert.AreEqual("FigureOne", _figureRepository.GetFiguresByClient("OwnerOne")[0].Name);
-			Assert.AreEqual("FigureTwo", _figureRepository.GetFiguresByClient("OwnerTwo")[0].Name);
+			Assert.AreEqual("FigureOne", _figureRepository.GetFiguresByClient(_owner)[0].Name);
+			Assert.AreEqual("FigureTwo", _figureRepository.GetFiguresByClient(_otherOwner)[0].Name);
 		}
 
 		[TestMethod]
 		public void GetFiguresByClient_NotExisting2Client()
 		{
-			_figureRepository.GetFiguresByClient("");
+			Client notExitingClient = new Client() { Username = "notExist" };
+
+			_figureRepository.GetFiguresByClient(notExitingClient);
 		}
 
 		[TestMethod]
@@ -89,7 +96,7 @@ namespace Test.MemoryRepositoryTest
 				Owner = _owner
 			};
 
-			_figureRepository.AddFigure(newFigure);
+			_figureRepository.AddFigure(newFigure, _owner);
 
 			List<Figure> iterable = _figureRepository.GetFiguresByClient(_owner);
 
@@ -106,7 +113,7 @@ namespace Test.MemoryRepositoryTest
 				Owner = _owner
 			};
 
-			_figureRepository.AddFigure(newFigure);
+			_figureRepository.AddFigure(newFigure, _owner);
 			_figureRepository.RemoveFigure(newFigure);
 			List<Figure> figures = _figureRepository.GetFiguresByClient(_owner);
 
