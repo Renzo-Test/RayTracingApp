9e1f91d28949203c65962911da11cfbab64c1250
Renzo-Test <renzotestorelli27@gmail.com>
2023-06-13 19:09:40 -0300

    RenderOutput added atributes

--- RayTracingApp/Engine/RenderOutput.cs
+++ RayTracingApp/Engine/RenderOutput.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,7 @@ namespace Engine
 {
 	public class RenderOutput
 	{
+		public Stopwatch RenderTime { get; set; }
+		public string RenderedImage{ get; set; }
 	}
 }
